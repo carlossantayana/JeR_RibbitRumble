bbedb615f0ab322dad9a4a1451eadeb194e7a7e9
Mdoc14 <marcos.deozaeta@gmail.com>
2023-12-07 17:52:51 +0100

    Botones menu implementados

--- Juego/MainMenu.js
+++ Juego/MainMenu.js
@@ -1,25 +1,57 @@
 "use strict";
 
 
-export default class MainMenu extends Phaser.Scene
-{
-    constructor ()
-    {
+export default class MainMenu extends Phaser.Scene {
+    constructor() {
         super('MainMenu');
     }
 
-    create ()
-    {
+    create() {
         this.add.image(940, 534.5, 'menuPrincipal').setScale(0.5);
 
-        const Button= this.add.image(400,400,'pruebaBoton'); //Objeto que queremos que sea el boton
-        Button.setInteractive(); //"setInteractive()" le da la posibilidad de ser interactivo
-        Button.on('pointerdown', () => this.actionOnClick()); //creamos un EventListener "pointerdown" (cuando se hace click con el ratón) que ejecute la función que queremos
+        const playButton = this.add.image(940, 560, 'pruebaBoton').setScale(0.25, 0.1).setInteractive(); //Objeto que queremos que sea el boton
+        const settingsButton = this.add.image(940, 640, 'pruebaBoton').setScale(0.25, 0.1).setInteractive(); //Objeto que queremos que sea el boton
+        const exitButton = this.add.image(940, 720, 'pruebaBoton').setScale(0.25, 0.1).setInteractive(); //Objeto que queremos que sea el boton
+
+        //creamos EventListeners "pointerdown" (cuando se hace click con el ratón) que ejecuten la función que queremos
+        playButton.on('pointerdown', () => this.playOnClick());
+        settingsButton.on('pointerdown', () => this.settingsOnClick());
+        exitButton.on('pointerdown', () => this.exitOnClick());
+
+        this.buttonOver(playButton);
+        this.buttonOver(settingsButton);
+        this.buttonOver(exitButton);
+
+        this.buttonOut(playButton);
+        this.buttonOut(settingsButton);
+        this.buttonOut(exitButton);
+
     }
 
-    actionOnClick () { //Esta es la función que hace el boton al pulsarse
+    playOnClick() { //Esta es la función que hace el boton de jugar al pulsarse
 
-        this.scene.start('PlayerSelectionMenu'); //Cargar Escena de selección de personaje
+        this.scene.start('PlayerSelectionMenu');
+    }
+    settingsOnClick() { //Esta es la función que hace el boton de ajustes al pulsarse
+
+        //this.scene.start('PlayerSelectionMenu'); 
+    }
+    exitOnClick() { //Esta es la función que hace el boton de salir al pulsarse
+        window.close();
+    }
+
+    buttonOver(button) {
+        button.on('pointerover', function(){
+            this.setTint(0x89716C);
+            this.setScale(0.26, 0.11);
+        })
+    }
+
+    buttonOut(button) {
+        button.on('pointerout', function(){
+            this.clearTint();
+            this.setScale(0.25, 0.1);
+        })
     }
 }
 
