fe91fb6824c9a16cd43b7987261eac96fbd93bae
Sebastian02L <sebaslf02@gmail.com>
2023-12-11 17:39:21 +0100

    Pantalla de Carga implementada

--- Juego/Preloader.js
+++ Juego/Preloader.js
@@ -7,6 +7,7 @@ export default class Preloader extends Phaser.Scene {
 
     preload()//Cargar imagenes y sonido
     {
+
         //ASSETS ANIMACIONES//
         this.load.setPath('assets/Characters');
         this.load.spritesheet('ToroIdle', 'RanaToro/ToroIdleV1.1.png', { frameWidth: 600, frameHeight: 293 });
@@ -16,7 +17,6 @@ export default class Preloader extends Phaser.Scene {
         this.load.spritesheet('ToroAgachado', 'RanaToro/ToroAgachadoV1.1.png', { frameWidth: 600, frameHeight: 293 });
         this.load.spritesheet('ToroBajo', 'RanaToro/ToroAtaqueBajoV1.1.png', { frameWidth: 600, frameHeight: 293 });
         this.load.spritesheet('ToroSalto', 'RanaToro/ToroSaltoV1.1.png', { frameWidth: 600, frameHeight: 293 });
-        this.load.spritesheet('ToroDaño', 'RanaToro/ToroDaño.png', { frameWidth: 600, frameHeight: 293 });
 
         this.load.spritesheet('LluviaIdle', 'RanaDeLluvia/Idle_Spritesheet.png', { frameWidth: 3508, frameHeight: 2480 });
         this.load.spritesheet('LluviaCaminar', 'RanaDeLluvia/Walk_Spritesheet.png', { frameWidth: 3508, frameHeight: 2480 });
@@ -25,7 +25,6 @@ export default class Preloader extends Phaser.Scene {
         this.load.spritesheet('LluviaAgachado', 'RanaDeLluvia/Crouch_Spritesheet.png', { frameWidth: 3508, frameHeight: 2480 });
         this.load.spritesheet('LluviaBajo', 'RanaDeLluvia/DownAttack_Spritesheet.png', { frameWidth: 3508, frameHeight: 2480 });
         this.load.spritesheet('LluviaSalto', 'RanaDeLluvia/Jump_Spritesheet.png', { frameWidth: 3508, frameHeight: 2480 });
-        this.load.spritesheet('LluviaDaño', 'RanaDeLluvia/Hurt_Sprite.png', { frameWidth: 3508, frameHeight: 2480 });
 
         this.load.spritesheet('FlechaIdle', 'RanaFlecha/FlechaIdle.png', { frameWidth: 301, frameHeight: 252 });
         this.load.spritesheet('FlechaCaminar', 'RanaFlecha/FlechaAndar.png', { frameWidth: 301, frameHeight: 252 });
@@ -34,16 +33,6 @@ export default class Preloader extends Phaser.Scene {
         this.load.spritesheet('FlechaAgachado', 'RanaFlecha/FlechaAgachar.png', { frameWidth: 301, frameHeight: 252 });
         this.load.spritesheet('FlechaBajo', 'RanaFlecha/FlechaAtaqueBajo.png', { frameWidth: 301, frameHeight: 252 });
         this.load.spritesheet('FlechaSalto', 'RanaFlecha/FlechaSalto.png', { frameWidth: 301, frameHeight: 252 });
-        this.load.spritesheet('FlechaDaño', 'RanaFlecha/FlechaDaño.png', { frameWidth: 300, frameHeight: 250 });
-
-        this.load.spritesheet('TrepadoraIdle', 'RanaTrepadora/spritesheet_Idle.png', { frameWidth: 2289, frameHeight: 1653 });
-        this.load.spritesheet('TrepadoraCaminar', 'RanaTrepadora/spritesheet_Walk.png', { frameWidth: 2289, frameHeight: 1653 });
-        this.load.spritesheet('TrepadoraBasico', 'RanaTrepadora/spritesheet_Attack.png', { frameWidth: 2289, frameHeight: 1653 });
-        this.load.spritesheet('TrepadoraBloqueo', 'RanaTrepadora/spritesheet_Block.png', { frameWidth: 2289, frameHeight: 1653 });
-        this.load.spritesheet('TrepadoraAgachado', 'RanaTrepadora/spritesheet_Crouch.png', { frameWidth: 2289, frameHeight: 1653 });
-        this.load.spritesheet('TrepadoraBajo', 'RanaTrepadora/spritesheet_CrouchAttack.png', { frameWidth: 2289, frameHeight: 1653 });
-        this.load.spritesheet('TrepadoraSalto', 'RanaTrepadora/spritesheet_Jump.png', { frameWidth: 2289, frameHeight: 1653 });
-        this.load.spritesheet('TrepadoraDaño', 'RanaTrepadora/RanaTrepadoraDamaged.png', { frameWidth: 2289, frameHeight: 1653 });
 
         //ASSETS FONDOS//
         this.load.setPath('assets/Backgrounds'); //ruta de los assets, para que no sea necesario poner dicha ruta
@@ -127,6 +116,7 @@ export default class Preloader extends Phaser.Scene {
 
     create() //crear animaciones y cargar escena inicial
     {
+
         //////////////////////////////////////////////RANA TORO ANIMACIONES////////////////////////////////////////////////////
 
         //Animacion Idle
@@ -153,7 +143,7 @@ export default class Preloader extends Phaser.Scene {
             repeat: 0
         });
 
-        //Animacion de ataque bajo
+        //Animación de ataque bajo
         this.anims.create({
             key: 'ToroDownAttackAnim',
             frames: this.anims.generateFrameNumbers('ToroBajo', { start: 0, end: 6 }),
@@ -169,13 +159,13 @@ export default class Preloader extends Phaser.Scene {
             repeat: 0
         });
 
-        //Animacion bloqueo fijo
+        //Aniamcion bloqueo fijo
         this.anims.create({
             key: 'ToroBlockAnim',
             frames: this.anims.generateFrameNumbers('ToroBloqueo', { start: 3, end: 3 }),
         });
 
-        //Animacion de terminar bloqueo
+        //Aniamcion de terminar bloqueo
         this.anims.create({
             key: 'ToroEndBlockAnim',
             frames: this.anims.generateFrameNumbers('ToroBloqueo', { start: 4, end: 8 }),
@@ -183,7 +173,7 @@ export default class Preloader extends Phaser.Scene {
             repeat: 0
         });
 
-        //Animacion comenzar agachado
+        //Animación comenzar agachado
         this.anims.create({
             key: 'ToroBeginCrouchAnim',
             frames: this.anims.generateFrameNumbers('ToroAgachado', { start: 0, end: 3 }),
@@ -191,13 +181,13 @@ export default class Preloader extends Phaser.Scene {
             repeat: 0
         });
 
-        //Animacion agachado fijo
+        //Aniamcion agachado fijo
         this.anims.create({
             key: 'ToroCrouchAnim',
             frames: this.anims.generateFrameNumbers('ToroAgachado', { start: 3, end: 3 }),
         });
 
-        //Animacion de terminar agachado
+        //Aniamcion de terminar agachado
         this.anims.create({
             key: 'ToroEndCrouchAnim',
             frames: this.anims.generateFrameNumbers('ToroAgachado', { start: 3, end: 6 }),
@@ -213,7 +203,6 @@ export default class Preloader extends Phaser.Scene {
             repeat: 0
         });
 
-        //Animacion salto fijo
         this.anims.create({
             key: 'ToroJumpAnim',
             frames: this.anims.generateFrameNumbers('ToroSalto', { start: 3, end: 3 }),
@@ -221,7 +210,6 @@ export default class Preloader extends Phaser.Scene {
             repeat: 0
         });
 
-        //Animacion terminar salto
         this.anims.create({
             key: 'ToroEndJumpAnim',
             frames: this.anims.generateFrameNumbers('ToroSalto', { start: 3, end: 5 }),
@@ -229,16 +217,7 @@ export default class Preloader extends Phaser.Scene {
             repeat: 0
         });
 
-        //Animacion daño
-        this.anims.create({
-            key: 'ToroHurtAnim',
-            frames: this.anims.generateFrameNumbers('ToroDaño', {start: 0, end: 0}),
-            frameRate: 12,
-            repeat: 12
-        });
-
         //////////////////////////////////////////////RANA DE LLUVIA ANIMACIONES////////////////////////////////////////////////////
-        
         //Animacion Idle
         this.anims.create({
             key: 'LluviaIdleAnim',
@@ -263,7 +242,7 @@ export default class Preloader extends Phaser.Scene {
             repeat: 0
         });
 
-        //Animacion de ataque bajo
+        //Animación de ataque bajo
         this.anims.create({
             key: 'LluviaDownAttackAnim',
             frames: this.anims.generateFrameNumbers('LluviaBajo', { start: 0, end: 12 }),
@@ -279,13 +258,13 @@ export default class Preloader extends Phaser.Scene {
             repeat: 0
         });
 
-        //Animacion bloqueo fijo
+        //Aniamcion bloqueo fijo
         this.anims.create({
             key: 'LluviaBlockAnim',
             frames: this.anims.generateFrameNumbers('LluviaBloqueo', { start: 2, end: 2 }),
         });
 
-        //Animacion de terminar bloqueo
+        //Aniamcion de terminar bloqueo
         this.anims.create({
             key: 'LluviaEndBlockAnim',
             frames: this.anims.generateFrameNumbers('LluviaBloqueo', { start: 2, end: 4 }),
@@ -293,7 +272,7 @@ export default class Preloader extends Phaser.Scene {
             repeat: 0
         });
 
-        //Animacion comenzar agachado
+        //Animación comenzar agachado
         this.anims.create({
             key: 'LluviaBeginCrouchAnim',
             frames: this.anims.generateFrameNumbers('LluviaAgachado', { start: 0, end: 2 }),
@@ -301,13 +280,13 @@ export default class Preloader extends Phaser.Scene {
             repeat: 0
         });
 
-        //Animacion agachado fijo
+        //Aniamcion agachado fijo
         this.anims.create({
             key: 'LluviaCrouchAnim',
             frames: this.anims.generateFrameNumbers('LluviaAgachado', { start: 2, end: 2 }),
         });
 
-        //Animacion de terminar agachado
+        //Aniamcion de terminar agachado
         this.anims.create({
             key: 'LluviaEndCrouchAnim',
             frames: this.anims.generateFrameNumbers('LluviaAgachado', { start: 2, end: 4 }),
@@ -323,7 +302,6 @@ export default class Preloader extends Phaser.Scene {
             repeat: 0
         });
 
-        //Animacion salto fijo
         this.anims.create({
             key: 'LluviaJumpAnim',
             frames: this.anims.generateFrameNumbers('LluviaSalto', { start: 3, end: 3 }),
@@ -331,20 +309,11 @@ export default class Preloader extends Phaser.Scene {
             repeat: 0
         });
 
-        //Animacion terminar salto
         this.anims.create({
             key: 'LluviaEndJumpAnim',
             frames: this.anims.generateFrameNumbers('LluviaSalto', { start: 3, end: 6 }),
             frameRate: 12,
             repeat: 0
-        });
-
-        //Animacion daño
-        this.anims.create({
-            key: 'LluviaHurtAnim',
-            frames: this.anims.generateFrameNumbers('LluviaDaño', {start: 0, end: 0}),
-            frameRate: 12,
-            repeat: 2
         });
 
         //////////////////////////////////////////////RANA FLECHA ANIMACIONES////////////////////////////////////////////////////
@@ -373,7 +342,7 @@ export default class Preloader extends Phaser.Scene {
             repeat: 0
         });
 
-        //Animacion de ataque bajo
+        //Animación de ataque bajo
         this.anims.create({
             key: 'FlechaDownAttackAnim',
             frames: this.anims.generateFrameNumbers('FlechaBajo', { start: 0, end: 6 }),
@@ -389,13 +358,13 @@ export default class Preloader extends Phaser.Scene {
             repeat: 0
         });
 
-        //Animacion bloqueo fijo
+        //Aniamcion bloqueo fijo
         this.anims.create({
             key: 'FlechaBlockAnim',
             frames: this.anims.generateFrameNumbers('FlechaBloqueo', { start: 4, end: 4 }),
         });
 
-        //Animacion de terminar bloqueo
+        //Aniamcion de terminar bloqueo
         this.anims.create({
             key: 'FlechaEndBlockAnim',
             frames: this.anims.generateFrameNumbers('FlechaBloqueo', { start: 4, end: 0 }),
@@ -403,7 +372,7 @@ export default class Preloader extends Phaser.Scene {
             repeat: 0
         });
 
-        //Animacion comenzar agachado
+        //Animación comenzar agachado
         this.anims.create({
             key: 'FlechaBeginCrouchAnim',
             frames: this.anims.generateFrameNumbers('FlechaAgachado', { start: 0, end: 2 }),
@@ -411,13 +380,13 @@ export default class Preloader extends Phaser.Scene {
             repeat: 0
         });
 
-        //Animacion agachado fijo
+        //Aniamcion agachado fijo
         this.anims.create({
             key: 'FlechaCrouchAnim',
             frames: this.anims.generateFrameNumbers('FlechaAgachado', { start: 2, end: 2 }),
         });
 
-        //Animacion de terminar agachado
+        //Aniamcion de terminar agachado
         this.anims.create({
             key: 'FlechaEndCrouchAnim',
             frames: this.anims.generateFrameNumbers('FlechaAgachado', { start: 2, end: 0 }),
@@ -433,7 +402,6 @@ export default class Preloader extends Phaser.Scene {
             repeat: 0
         });
 
-        //Animacion salto fijo
         this.anims.create({
             key: 'FlechaJumpAnim',
             frames: this.anims.generateFrameNumbers('FlechaSalto', { start: 5, end: 5 }),
@@ -441,131 +409,15 @@ export default class Preloader extends Phaser.Scene {
             repeat: 0
         });
 
-        //Animacion terminar salto
         this.anims.create({
             key: 'FlechaEndJumpAnim',
             frames: this.anims.generateFrameNumbers('FlechaSalto', { start: 5, end: 1 }),
             frameRate: 18,
             repeat: 0
         });
-
-        //Animacion daño
-        this.anims.create({
-            key: 'FlechaHurtAnim',
-            frames: this.anims.generateFrameNumbers('FlechaDaño', {start: 0, end: 0}),
-            frameRate: 12,
-            repeat: 2
-        });
-
         //////////////////////////////////////////////RANA TREPADORA ANIMACIONES////////////////////////////////////////////////////
 
-        //Animacion Idle
-        this.anims.create({
-            key: 'TrepadoraIdleAnim',
-            frames: this.anims.generateFrameNumbers('TrepadoraIdle', {start: 0, end: 8}),
-            frameRate: 12,
-            repeat: -1
-        });
 
-        //Animacion de caminar
-        this.anims.create({
-            key: 'TrepadoraWalkAnim',
-            frames: this.anims.generateFrameNumbers('TrepadoraCaminar', { start: 0, end: 8 }),
-            frameRate: 12,
-            repeat: 0
-        });
-
-        //Animacion de ataque basico
-        this.anims.create({
-            key: 'TrepadoraBasicAttackAnim',
-            frames: this.anims.generateFrameNumbers('TrepadoraBasico', { start: 0, end: 9 }),
-            frameRate: 12,
-            repeat: 0
-        });
-
-        //Animacion de ataque bajo
-        this.anims.create({
-            key: 'TrepadoraDownAttackAnim',
-            frames: this.anims.generateFrameNumbers('TrepadoraBajo', { start: 0, end: 9 }),
-            frameRate: 12,
-            repeat: 0
-        });
-
-        //Animacion de empezar bloqueo
-        this.anims.create({
-            key: 'TrepadoraBeginBlockAnim',
-            frames: this.anims.generateFrameNumbers('TrepadoraBloqueo', { start: 0, end: 3 }),
-            frameRate: 12,
-            repeat: 0
-        });
-
-        //Animacion bloqueo fijo
-        this.anims.create({
-            key: 'TrepadoraBlockAnim',
-            frames: this.anims.generateFrameNumbers('TrepadoraBloqueo', { start: 3, end: 3 }),
-        });
-
-        //Animacion de terminar bloqueo
-        this.anims.create({
-            key: 'TrepadoraEndBlockAnim',
-            frames: this.anims.generateFrameNumbers('TrepadoraBloqueo', { start: 3, end: 0 }),
-            frameRate: 12,
-            repeat: 0
-        });
-
-        //Animacion comenzar agachado
-        this.anims.create({
-            key: 'TrepadoraBeginCrouchAnim',
-            frames: this.anims.generateFrameNumbers('TrepadoraAgachado', { start: 0, end: 4 }),
-            frameRate: 12,
-            repeat: 0
-        });
-
-        //Animacion agachado fijo
-        this.anims.create({
-            key: 'TrepadoraCrouchAnim',
-            frames: this.anims.generateFrameNumbers('TrepadoraAgachado', { start: 4, end: 4 }),
-        });
-
-        //Animacion de terminar agachado
-        this.anims.create({
-            key: 'TrepadoraEndCrouchAnim',
-            frames: this.anims.generateFrameNumbers('TrepadoraAgachado', { start: 4, end: 0 }),
-            frameRate: 12,
-            repeat: 0
-        });
-
-        //Animacion comenzar salto
-        this.anims.create({
-            key: 'TrepadoraBeginJumpAnim',
-            frames: this.anims.generateFrameNumbers('TrepadoraSalto', { start: 0, end: 6 }),
-            frameRate: 12,
-            repeat: 0
-        });
-
-        //Animacion salto fijo
-        this.anims.create({
-            key: 'TrepadoraJumpAnim',
-            frames: this.anims.generateFrameNumbers('TrepadoraSalto', { start: 6, end: 6 }),
-            frameRate: 12,
-            repeat: 0
-        });
-
-        //Animacion terminar salto
-        this.anims.create({
-            key: 'TrepadoraEndJumpAnim',
-            frames: this.anims.generateFrameNumbers('TrepadoraSalto', { start: 6, end: 0 }),
-            frameRate: 12,
-            repeat: 0
-        });
-
-        //Animacion daño
-        this.anims.create({
-            key: 'TrepadoraHurtAnim',
-            frames: this.anims.generateFrameNumbers('TrepadoraDaño', {start: 0, end: 0}),
-            frameRate: 12,
-            repeat: 2
-        });
 
         this.scene.start('MainMenu'); //Cargar Escena inicial
     }
