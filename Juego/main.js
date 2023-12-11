a833b0b8741758a22ec8bb1e44860e580ebad28e
Sebastian02L <sebaslf02@gmail.com>
2023-12-11 17:51:53 +0100

    Revert "Pantalla de Carga implementada"
    This reverts commit fe91fb6824c9a16cd43b7987261eac96fbd93bae.

--- Juego/main.js
+++ Juego/main.js
@@ -1,6 +1,5 @@
 "use strict";
 
-import Charging from './Charging.js';
 import Preloader from './Preloader.js';
 import MainMenu from './MainMenu.js';
 import SettingsMenu from './SettingsMenu.js';
@@ -16,7 +15,7 @@ const config = { //Configuración general
     width: 1920,
     height: 1080,
     backgroundColor: '#000000',
-    scene: [ Charging, Preloader, MainMenu, SettingsMenu, AudioManager, ModeSelectionMenu, PlayerSelectionMenu, MapSelectionMenu, Game, Results], //Escenas, Preloader está primero, se cargara Preloader primero
+    scene: [ Preloader, MainMenu, SettingsMenu, AudioManager, ModeSelectionMenu, PlayerSelectionMenu, MapSelectionMenu, Game, Results], //Escenas, Preloader está primero, se cargara Preloader primero
     scale:{
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH
