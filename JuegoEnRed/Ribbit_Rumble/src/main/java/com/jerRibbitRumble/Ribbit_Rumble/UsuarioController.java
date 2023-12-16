53ce4dc1c55feba6ff676fa76ba160daac0e6205
Mdoc14 <marcos.deozaeta@gmail.com>
2023-12-16 20:07:52 +0100

    Lectura y escritura en fichero

--- JuegoEnRed/Ribbit_Rumble/src/main/java/com/jerRibbitRumble/Ribbit_Rumble/UsuarioController.java
+++ JuegoEnRed/Ribbit_Rumble/src/main/java/com/jerRibbitRumble/Ribbit_Rumble/UsuarioController.java
@@ -1,11 +1,17 @@
 package com.jerRibbitRumble.Ribbit_Rumble;
 
+import java.io.*;
+
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -20,58 +26,83 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RestController
 @RequestMapping("/Usuarios")
 public class UsuarioController {
-	
-	private Map<Long,Usuario> users = new ConcurrentHashMap<>();
+
+	private Map<Long, Usuario> users = new ConcurrentHashMap<>();
 	private AtomicLong lastId = new AtomicLong();
-	
+
+	String nombreArchivo = "usuariosMap.txt";
+
 	@PostMapping(value = "/")
 	@ResponseStatus(HttpStatus.CREATED)
 	public Usuario createUsuario(@RequestBody Usuario user) {
 		long id = lastId.getAndIncrement();
-		
+
 		user.setId(id);
 		users.put(id, user);
-		
+
 		return user;
 	}
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Usuario>getUsuario(@PathVariable long id){
+	public ResponseEntity<Usuario> getUsuario(@PathVariable long id) {
 		Usuario user = users.get(id);
-		
-		if(user != null) {
-			return new ResponseEntity<>(user,HttpStatus.OK);
-		}else {
+
+		if (user != null) {
+			return new ResponseEntity<>(user, HttpStatus.OK);
+		} else {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 	}
-	
+
 	@GetMapping(value = "/")
-	public Collection<Usuario> getUsuarios(){
+	public Collection<Usuario> getUsuarios() {
 		return users.values();
 	}
-	
+
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<Usuario> updateUsuario(@PathVariable long id, @RequestBody Usuario userUpdated){
+	public ResponseEntity<Usuario> updateUsuario(@PathVariable long id, @RequestBody Usuario userUpdated) {
 		Usuario user = users.get(id);
-		
-		if(user != null) {
+
+		if (user != null) {
 			userUpdated.setId(id);
 			users.put(null, userUpdated);
-			return new ResponseEntity<>(userUpdated,HttpStatus.OK);
-		}else {
+			return new ResponseEntity<>(userUpdated, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Usuario> deleteUsuario(@PathVariable long id) {
+		Usuario user = users.remove(id);
+
+		if (user != null) {
+			return new ResponseEntity<>(user, HttpStatus.OK);
+		} else {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 	}
 	
-	@DeleteMapping(value = "/{id}")
-	public ResponseEntity<Usuario> deleteUsuario(@PathVariable long id){
-		Usuario user = users.remove(id);
-		
-		if(user != null) {
-			return new ResponseEntity<>(user, HttpStatus.OK);
-		}else {
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		}
+	@PreDestroy
+	public void escribirFichero() {
+	    try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(nombreArchivo))) {
+	        outputStream.writeObject(users);
+	        System.out.println("HashMap guardado en el archivo '" + nombreArchivo + "' correctamente.");
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+	}
+	
+	@PostConstruct
+	public void leerFichero() {
+	    try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(nombreArchivo))) {
+	        users = (Map<Long, Usuario>) inputStream.readObject();
+	        System.out.println("Map leído desde el archivo '" + nombreArchivo + "':");
+	        for (Long key : users.keySet()) {
+	            System.out.println("Key: " + key + ", Value: " + users.get(key));
+	        }
+	    } catch (IOException | ClassNotFoundException e) {
+	        e.printStackTrace();
+	    }
 	}
 }
