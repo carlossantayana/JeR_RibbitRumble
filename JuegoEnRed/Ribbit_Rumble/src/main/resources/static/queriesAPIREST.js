d9dab579d9f51d247fe360b3b350fb2d0b59d8f5
Sebastian02L <sebaslf02@gmail.com>
2023-12-17 03:05:35 +0100

    Intento de login de usuario 2

--- JuegoEnRed/Ribbit_Rumble/src/main/resources/static/queriesAPIREST.js
+++ JuegoEnRed/Ribbit_Rumble/src/main/resources/static/queriesAPIREST.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
     var chatbox = $('#chatBox')
     var message = $('#input-message')
     var logedUserId; //ID del usuario conectado
+    var access = false;
 
     //Metodo que controla el click sobre el boton de enviar mensaje
     $("#sendChat").click(function() {
@@ -12,39 +13,87 @@ $(document).ready(function(){
         message.val(''); // Limpiar el input después de enviar el mensaje
     })
 
-    var acces=false;
-    while(!acces){
-        if(GetNumberOfUsers()==0){
-            alert("No existen usuarios, crea uno");
-        }
-        if (GetNumberOfUsers()>1 && confirm("¿Quieres entrar con un usuario existente?, sino tendrás que crearlo."))
-        {
-            while(!acces){
-                var nombre
-                var pasword
+    if(!access){
+        
+        //Utilizamos un callback para obtener el numero de usuarios en el servidor
+        GetNumberOfUsers(function(numberOfUsers){
+            console.log(numberOfUsers)
+        if( numberOfUsers === 0 ){
+            alert("No existen usuarios en el servidor, crea uno");
 
-                while(nombre===null || nombre===undefined){
+            //Proceso de creación del primer usuario del servidor
+            while(!access){
+                var nombre
+                var password
+
+                nombre = prompt("Nombre de usuario:","")
+                //Si deja el espacio vacio
+                while(nombre === null || nombre === undefined){
                     nombre = prompt("Nombre de usuario:","")
                 }
-                while(pasword===null|| pasword===undefined){
-                    pasword = prompt("Contraseña","")
+                pasword = prompt("Contraseña","")
+                //Si deja el espacio vacio
+                while(password === null|| password === undefined){
+                    password = prompt("Contraseña","")
                 }
-                //Se comprueba si existe un usuario con el nombre y contraseña correctos
-                if(!SearchForUser(false,nombre,pasword)){
-                    alert("Nombre o contraseña incorrectos");
-                }
-                else{
-                    logedUserId=GetUserId(nombre,pasword);
-                    acces=true;
-                }
+                //Llamada al metodo de crear usuario
+                registerUser(nombre, password)
+                access = true;
             }
         }
-        else
+    });
+    //En el caso de que si haya usuarios creados en el servidor
+    }  
+
+    
+    if(!access){
+        //Si confirma que quiere entrar con un usuario existente
+        if (confirm("¿Quieres entrar con un usuario existente?, sino tendrás que crearlo."))
+        {
+            while(!access){
+                var nombre
+                var password
+
+                nombre = prompt("Nombre de usuario:","")
+                while(nombre === null || nombre === undefined){
+                    nombre = prompt("Nombre de usuario:","")
+                }
+                pasword = prompt("Contraseña","")
+                while(password === null|| password === undefined){
+                    password = prompt("Contraseña","")
+                }
+                console.log("Usuario Introducido: " + nombre + "PW Introducido: " + password)
+                //Se comprueba si existe un usuario con el nombre y contraseña correctos
+                GetUsers(function(Users){
+                    console.log("Entré en getUsers")
+                    for(var i = 0; i < Users.length; i++){
+                        if(username === Users[i].username && password === Users[i].password){
+                            console.log("Inicio de sesion exitoso")
+                            access = true;
+                            break;
+                        }
+                    }
+                    alert("Nombre o contraseña incorrectos despues de buscar");
+                });
+
+                //COSAS RARAS
+               /* if(!SearchForUser(false, nombre, password)){
+                    console.log(SearchForUser(false, nombre, password))
+                    alert("Nombre o contraseña incorrectos despues de buscar");
+                }
+                else{
+                    console.log("Inicio de sesion exitoso")
+                    //logedUserId=GetUserId(nombre,password);
+                    access = true;
+                }*/
+            }
+        }
+        /*else
         {
             if(GetNumberOfUsers()===0){
                 alert("No existen usuarios, crea uno");
             }
-            while(!acces)
+            while(!access)
             {
                 var pasword;
                 var nombre
@@ -65,7 +114,7 @@ $(document).ready(function(){
                     }
                     else
                     {
-                        acces=true;
+                        access = true;
                     }
                 }
 
@@ -73,10 +122,10 @@ $(document).ready(function(){
             alert("Nuevo usuario creado")
             registerUser(nombre,pasword);
             logedUserId=GetUserId(nombre,pasword);
-        }  
+        } */
     }
 
-    });
+    }); //Fin del documento.ready
 
     //Funcion que se llama cuando se va a registrar un usuario y hace el POST al servidor
     function registerUser(usernameP, passwordP){
@@ -108,26 +157,64 @@ $(document).ready(function(){
         });
     }
 
-    function SearchForUser(onlyName,username,password) {
+    //Funcion que busca un usuario ya creado
+    function SearchForUser(onlyName, username, password, callback) {
+        var found;
+        console.log("Entré en searchForUser")
+
+        //Obtenemos a todos los usuarios del servidor
         $.ajax({
             url: "http://localhost:8080/Usuarios/"
         }).done(function(data){
+            for(var i = 0; i < data.length; i++){
+
+                if(username === data[i].username && password === data[i].password)
+                console.log("Usuario y contraseña correctas");
+                //Retornamos true para salir del bucle
+                found = true;
+            }
+            found = false;
+        });
+        return found;
+        // PARA BORRAR //
+        //Conseguimos todos los usuarios del servidor con un callback y comprobamos si hay una coincidencia
+        /*GetUsers(function(Users){
+            console.log("Usuarios devueltos por el callback" + Users)
+            for(var i = 0; i < Users.length; i++){
+
+                //Si hay una coincidencia entre nombre y contraseña
+                if(username === Users[i].username && password === Users[i].password){
+                    console.log(Users[i].username + "y" + Users[i].password)
+                    console.log("Usuario y contraseña correctas");
+                    //Retornamos true para salir del bucle
+                    found = true;
+                } 
+            }
+            //Si no hay coincidencias, se deja en falso
+            found = false;
+        });*/
+       /* $.ajax({
+            url: "http://localhost:8080/Usuarios/"
+        }).done(function(data){
             for(var i = 0; i < data.items.length; i++){
+
                 if(onlyName){
-                    if(data.item[i].username===username)
+                    if(data.item[i].username === username)
                     {
+                        console.log("Usuario Encontrado")
                         return true;
                     }
                 }
                 else{
-                    if(data.item[i].username===username && data.items[i].password===password)
+                    if(data.item[i].username === username && data.items[i].password === password)
                     {
+                        console.log("Usuario y contraseña encontrada")
                         return true;
                     }
                 }
             }
             return false;
-        });
+        });*/
     };
 
     function GetUserId(username,password) {
@@ -144,13 +231,33 @@ $(document).ready(function(){
         });
     };
 
-    function GetNumberOfUsers() {
+    //Funcion que devuelve el número de usuarios almacenados en el servidor
+    function GetNumberOfUsers(callback) {
+        $.ajax({
+            method: "GET",
+            url: "http://localhost:8080/Usuarios/"
+        }).done(function(data) {
+            // Llamamos al callback con el número de usuarios (esto soluciona la no sincronizacion del AJAX)
+            console.log("Numero de usuarios devuelto exitosamente")
+            callback(data.length);
+
+        }).fail(function(){
+            console.log("No se pudo conseguir el numero de usuarios")
+        });
+    }
+
+    
+    function GetUsers(callback2){
         $.ajax({
             url: "http://localhost:8080/Usuarios/"
-        }).done(function(data){
-            return data.items.length;
+        }).done(function(data) {
+            // Llamamos al callback con los usuarios (esto soluciona la no sincronizacion del AJAX)
+            console.log("Datos devueltos: " + data)
+            callback2(data);
+        }).fail(function(){
+            console.log("No se pudo devolver los usuarios del servidor")
         });
-    };
+    }
 
 
 
