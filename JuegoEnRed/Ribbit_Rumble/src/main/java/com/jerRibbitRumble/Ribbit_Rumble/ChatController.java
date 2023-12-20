5ce8616778352cc5d54112bbf43a9847260716c0
Sebastián Loges <sebaslf02@gmail.com>
2023-12-20 14:55:46 +0100

    Elementos agregados al ChatController

--- JuegoEnRed/Ribbit_Rumble/src/main/java/com/jerRibbitRumble/Ribbit_Rumble/ChatController.java
+++ JuegoEnRed/Ribbit_Rumble/src/main/java/com/jerRibbitRumble/Ribbit_Rumble/ChatController.java
@@ -20,9 +20,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 @RequestMapping("/Chat")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ChatController {
 	private Map<Long, Mensaje> chatMessages = new ConcurrentHashMap<>();
 	
