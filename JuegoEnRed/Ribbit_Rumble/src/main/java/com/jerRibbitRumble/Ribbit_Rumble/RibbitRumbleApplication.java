2189ddf040dd434a671ded0adea7533469550d23
Mdoc14 <marcos.deozaeta@gmail.com>
2024-01-15 21:33:05 +0100

    Inicio de implementación websockets en server

--- JuegoEnRed/Ribbit_Rumble/src/main/java/com/jerRibbitRumble/Ribbit_Rumble/RibbitRumbleApplication.java
+++ JuegoEnRed/Ribbit_Rumble/src/main/java/com/jerRibbitRumble/Ribbit_Rumble/RibbitRumbleApplication.java
@@ -2,12 +2,26 @@ package com.jerRibbitRumble.Ribbit_Rumble;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @SpringBootApplication
-public class RibbitRumbleApplication {
+@EnableWebSocket
+public class RibbitRumbleApplication implements WebSocketConfigurer{
 
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+			registry.addHandler(Handler(), "/ribbits").setAllowedOrigins("*");
+	}
+	
+	@Bean
+	public WebSocketHandler Handler() {
+	return new WebSocketHandler();
+	}
+		
 	public static void main(String[] args) {
 		SpringApplication.run(RibbitRumbleApplication.class, args);
 	}
-
 }
---
+++ JuegoEnRed/Ribbit_Rumble/src/main/java/com/jerRibbitRumble/Ribbit_Rumble/WebSocketHandler.java
@@ -0,0 +1,30 @@
+package com.jerRibbitRumble.Ribbit_Rumble;
+
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+public class WebSocketHandler extends TextWebSocketHandler{
+	
+	private Map<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		System.out.println("New session: " + session.getId());
+		sessions.put(session.getId(), session);
+	}
+	
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+		System.out.println("Session closed: " + session.getId());
+		sessions.remove(session.getId());
+	}
+	
+	@Override
+	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+	}
+}
